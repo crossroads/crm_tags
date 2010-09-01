79101aeca5d7889ddc5177b03b5a6275bc2e7b4b
Nathan B <nathan.f77@gmail.com>
2010-09-01 13:16:09 +0800

    Refactored tag view hook into a partial

--- lib/crm_tags/tag_view_hooks.rb
+++ lib/crm_tags/tag_view_hooks.rb
@@ -1,43 +1,5 @@
 class TagViewHooks < FatFreeCRM::Callback::Base
 
-  ActsAsTaggableOn::Tag.all
-
-  TAGS_FIELD = <<EOS
-- asset = params[:controller].singularize
-- # Build asset tags manually in case the asset validation failed.
-- if params[asset] && params[asset][:tag_list]
-  - f.object.tags = params[asset][:tag_list].split(",").map {|x| ActsAsTaggableOn::Tag.find_by_name(x.strip)}.compact.uniq
-%tr
-  %td{ :valign => :top, :colspan => span }
-    .label.req Tags: <small>(comma separated, letters and digits only)</small>
-    %dd#facebook-list
-      = f.text_field :tag_list, :id => "tag_list", :style => "width:500px", :autocomplete => "off"
-      #facebook-auto
-        .default Type the name of a tag you'd like to use.  Use commas to separate multiple tags.
-        %ul.feed
-          - # Get tags from either the unsaved params, or from the object.
-          - tags = (params[asset] && params[asset][:tag_list]) ? params[asset][:tag_list].split(",") : f.object.tags.map{|t| t.name }
-          - tags.each do |tag|
-            %li{ :value => tag }= tag
-        :javascript
-          fbtaglist = new FacebookList('tag_list', 'facebook-auto',
-                                      { newValues: true,
-                                        regexSearch: false,
-                                        separator: Event.KEY_COMMA });
-          var tagjson = #{ActsAsTaggableOn::Tag.all.map{|t| {"caption" => t.name, "value" => t.name} }.to_json}
-          tagjson.each(function(t){fbtaglist.autoFeed(t)});
-EOS
-
-  #----------------------------------------------------------------------------
-  def javascript_includes(view, context = {})
-    includes = ""
-    # Load facebooklist.js for tag input
-    includes << view.javascript_include_tag("/plugin_assets/crm_tags/javascripts/facebooklist.js")
-    # No reason we cant put a stylesheet here too...
-    includes << view.stylesheet_link_tag("/plugin_assets/crm_tags/stylesheets/facebooklist.css")
-    includes
-  end
-
   TAGS_FOR_INDEX = <<EOS
 %dt
   .tags= tags_for_index(model)
@@ -83,10 +45,17 @@ EOS
   end
 
   #----------------------------------------------------------------------------
+  def javascript_includes(view, context = {})
+    # Load facebooklist.js for tag input (No reason we cant put the stylesheet here too...)
+    includes =  view.javascript_include_tag("/plugin_assets/crm_tags/javascripts/facebooklist.js")
+    includes << view.stylesheet_link_tag("/plugin_assets/crm_tags/stylesheets/facebooklist.css")
+  end
+
+  #----------------------------------------------------------------------------
   [ :account, :campaign, :contact, :lead, :opportunity ].each do |model|
 
     define_method :"#{model}_top_section_bottom" do |view, context|
-      Haml::Engine.new(TAGS_FIELD).render(view, :f => context[:f], :span => (model != :campaign ? 3 : 5))
+      view.render :partial => "/common/tags", :locals => {:f => context[:f], :span => (model != :campaign ? 3 : 5)}
     end
 
     define_method :"#{model}_bottom" do |view, context|
