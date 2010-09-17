d66918c8edf2b3d01b7424ca6e7a0244218baff9
Ben Tillman <bstillman@crossroadsint.org>
2010-09-17 21:39:27 +0800

    Removed simple_column_search dependency

--- init.rb
+++ init.rb
@@ -5,9 +5,9 @@ FatFreeCRM::Plugin.register(:crm_sample_tabs, self) do
        authors "Michael Dvorkin, Jose Luis Gordo Romero"
        version "0.2"
    description "Adds tagging support to Fat Free CRM"
-  dependencies :"acts-as-taggable-on", :haml, :simple_column_search
+  dependencies :"acts-as-taggable-on", :haml
 end
 
 require "crm_tags"
 
-Rails.configuration.middleware.insert_before ::Rack::Lock, ::ActionDispatch::Static, "#{root}/public"
+Rails.configuration.middleware.insert_before ::Rack::Lock, ::ActionDispatch::Static, root.join('public')
